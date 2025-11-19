65fa57e080a584431a367df5b3d042a9c6dc7faa
Yvan Acosta <acostacarrillo.yvan@gmail.com>
2025-11-18 22:26:10 -0700

    Boton y vista de edicion de articulo

--- trueque360/frontend/src/App.jsx
+++ trueque360/frontend/src/App.jsx
@@ -14,6 +14,7 @@ import TradesTab from './components/TradesTab/TradesTab';
 // --- 2. IMPORTAMOS LA NUEVA PÁGINA DE DETALLES Y SU CSS ---
 import ArticleDetail from './components/ArticleDetail/ArticleDetail';
 import './components/ArticleDetail/ArticleDetail.css';
+import EditArticle from './components/ArticleDetail/EditArticle';
 
 // --- Importamos el CSS del formulario (que faltaba) ---
 import './components/CreateArticle/CreateArticle.css';
@@ -104,7 +105,16 @@ function App() {
             path="/articulo/:id" 
             element={<ArticleDetail />} 
           />
-          
+          {/* Ruta para editar un artículo */}
+          <Route 
+            path="/articulo/editar/:id"
+            element={
+              <PrivateRoute>
+                <EditArticle />
+              </PrivateRoute>
+            }
+          />
+
           {/* --- Ruta 404 (Wildcard) --- */}
           {/* Si no encuentra ninguna ruta, muestra esto */}
           <Route 
